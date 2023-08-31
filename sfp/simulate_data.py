43ee81ededd8e99b2fca3ab1060d1c6922cd2840
Jiyeong Ha <jiyeong.ha@nyu.edu>
2023-08-31 12:20:00 -0400

    after VSS

--- sfp/simulate_data.py
+++ sfp/simulate_data.py
@@ -5,7 +5,6 @@ import matplotlib as mpl
 # we do this because sometimes we run this without an X-server, and this backend doesn't need
 # one. We set warn=False because the notebook uses a different backend and will spout out a big
 # warning to that effect; that's unnecessarily alarming, so we hide it.
-mpl.use('svg', warn=False)
 import argparse
 import pandas as pd
 import numpy as np
